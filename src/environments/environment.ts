4c58603d292676afc7cb1fff44865215e86cd6e5
Alfian Rikzandi <denydirgantara94@gmail.com>
2019-07-31 05:40:55 +0700

    31/07/19

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
 
   // LOCALHOST
-  base_api_url: '//project-manga.oo/',
-  base_api_version: 'v1',
-  base_api_key: '',
-
-  // HEROKU
-  // base_api_url: '//api-manga-arp.herokuapp.com/',
+  // base_api_url: '//project-manga.oo/',
   // base_api_version: 'v1',
   // base_api_key: '',
+
+  // HEROKU
+  base_api_url: '//api-manga-arp.herokuapp.com/',
+  base_api_version: 'v1',
+  base_api_key: '',
 
   // API UMBRELLA
   // base_api_url: 'https://api-1.arproject.web.id/',
